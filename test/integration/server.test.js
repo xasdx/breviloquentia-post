473f44c7d5d1e94236f406e90604d1d9499c4b46
_asdx_ <xasdx@tuta.io>
2018-01-26 11:30:58 +0000

    Cover WebServer with tests

---
+++ test/integration/server.test.js
@@ -0,0 +1,36 @@
+import { expect } from "chai"
+import express from "express"
+import WebServer from "~/adapter/rest/server"
+
+let environment = (port) => {
+	return {
+		web: {
+			app: {
+				port: port || 3210
+			}
+		}
+	}
+}
+
+
+export default {
+	"start": {
+		"starts a webserver": (f) => {
+			let server = new WebServer({ requestListener: () => express() }, environment())
+			server.start(() => {
+				server.stop(() => f())
+			})
+		},
+		"throws an error when something bad happens": () => {
+			let server = new WebServer({ requestListener: () => express() }, environment(-1))
+			expect(server.start).to.throw()
+		}
+	},
+	"server": {
+		"returns the http.Server instance": (f) => {
+			let server = new WebServer({ requestListener: () => express() }, environment())
+			expect(server.server().listen).to.be.a("function")
+			server.stop(() => f())
+		}
+	}
+}
